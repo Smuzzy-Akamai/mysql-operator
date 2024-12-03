577414cf94d77fb4424498ff7b51971b004263ab
Andrey Hristov <andrey.hristov@oracle.com>
2024-12-03 15:47:22 +0200

    Add an option in the test suite to kill pods
    Change-Id: I2cc9b244de2e91a8c299a2447c294634e2306e95

--- tests/utils/kutil.py
+++ tests/utils/kutil.py
@@ -517,7 +517,7 @@ def describe_cj(ns, name, cmd_output_log=KubectlCmdOutputLogging.DIAGNOSTICS):
 
 #
 
-def delete(ns, rsrc, name, timeout, wait=True):
+def delete(ns, rsrc, name, timeout, wait=True, additional_args=[]):
     if not name:
         name = "--all"
     args = []
@@ -526,7 +526,7 @@ def delete(ns, rsrc, name, timeout, wait=True):
     if not wait:
         args += ["--wait=false"]
 
-    kubectl("delete", rsrc, [name] + args, timeout=timeout, ignore=["NotFound"], timeout_diagnostics=lambda: store_diagnostics(ns, rsrc, name))
+    kubectl("delete", rsrc, [name] + args + additional_args, timeout=timeout, ignore=["NotFound"], timeout_diagnostics=lambda: store_diagnostics(ns, rsrc, name))
 
 
 def delete_ic(ns, name, timeout=600):
@@ -543,8 +543,11 @@ def delete_mbks(ns, prefix, timeout=240):
             delete_mbk(ns, mbk["NAME"], timeout)
 
 
-def delete_po(ns, name, timeout=240):
-    delete(ns, "po", name, timeout=timeout)
+def delete_po(ns, name, timeout=240, force=False):
+    additional_args = []
+    if force:
+        additional_args += ["--force", "--grace-period", "0"]
+    delete(ns, "po", name, timeout=timeout, additional_args=additional_args)
 
 
 def delete_sts(ns, name, timeout=5):
